7e4a8854a530660a2547acf8fb5f31435326fdb4
Vivek Kumar Vinay <vivekvinay96254@gmail.com>
2025-04-12 02:00:47 +0530

    changed models

--- Advanced-ATS-Resume-Checker/app2.py
+++ Advanced-ATS-Resume-Checker/app2.py
@@ -22,7 +22,7 @@ def generate_study_plan_with_gemini(role, weeks):
     """
 
     try:
-        model = genai.GenerativeModel("models/gemini-2.5-pro-exp-03-25")  
+        model = genai.GenerativeModel("gemini-1.5-flash")  
         response = model.generate_content(prompt)
         return response.text if hasattr(response, 'text') else str(response)
     except Exception as e:
